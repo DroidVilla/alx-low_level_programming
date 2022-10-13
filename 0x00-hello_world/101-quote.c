11ba8cafffae408a2e8612475520a7587a961b6b
DroidVilla <josyclassick@gmail.com>
2022-10-13 16:26:50 -0700

    Last for the night

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,17 +3,17 @@
 #include <unistd.h>
 
 /**
+
 * main - prints exactly "and that piece of art is useful"
 * - Dora Korpar, 2015-10-19",
 * followed by a new line, to the standard error.
-* Return: always 0 (Success)  
+* Return: Always 0 (Success)
 */
 
 int main(void)
 
 {
-
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
 
+return (1);
 }
