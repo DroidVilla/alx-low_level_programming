9272f0d04ba637e236a0a2db38ce735c89f4ddb6
DroidVilla <josyclassick@gmail.com>
2022-10-18 17:01:51 -0700

    Done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,8 +6,7 @@
 * Return: Always 0
 */
 
-int main(void)
-  
+int main(void)  
 {
 _putchar('_');
 _putchar('p');
@@ -19,5 +18,5 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 
-  return (0); 
+return (0); 
 }
