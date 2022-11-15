31a8ca8d5e7903147cbe98621b2f5fe3e210577e
DroidVilla <josyclassick@gmail.com>
2022-11-15 01:44:13 -0800

    Last try

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,46 +1,18 @@
-#include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include "dog.h"
+
 /**
- * new_dog - new dog
- * @name: name's dog
- * @age: age's dog
- * @owner: owner's dog
- * Return: newdog
+ * free_dog - frees dogs
+ * @d: pointer to dog to free
+ *
+ * Return: void
  */
-dog_t *new_dog(char *name, float age, char *owner)
+void free_dog(dog_t *d)
 {
-int i = 0, j = 0, k;
-dog_t *doge;
-while (name[i] != '\0')
-i++;
-while (owner[j] != '\0')
-j++;
-doge = malloc(sizeof(dog_t));
-if (doge == NULL)
-{
-free(doge);
-return (NULL);
-}
-doge->name = malloc(i *sizeof(doge->name));
-if (doge->name == NULL)
-{
-free(doge->name);
-free(doge);
-return (NULL);
-}
-for (k = 0; k <= i; k++)
-doge->name[k] = name[k];
-doge->age = age;
-doge->owner = malloc(j *sizeof(doge->owner));
-if (doge->owner == NULL)
-{
-free(doge->owner);
-free(doge->name);
-free(doge);
-return (NULL);
-}
-for (k = 0; k <= j; k++)
-doge->owner[k] = owner[k];
-return (doge);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
